0a6c7af479f982c1d3d69bd9a56f53c411127ef6
nickus1028 <bentivolio@gmail.com>
2022-02-27 14:19:00 -0500

    Added inquirer and initial questiosn with functions

---
+++ server.js
@@ -0,0 +1,62 @@
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const connection = require('./db/database');
+const { getDepartments, getEmployees, getRoles } = require('./db/query.js')
+
+function startEmployeeTracker() {
+    inquirer
+    .prompt({
+      type: "list",
+      name: "search",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "Exit"
+    ]
+  
+}).then (function (answer) {
+      switch (answer.search) {
+            case "View all departments":
+              viewAllDepartments();
+              break;
+  
+            case "View all roles":
+              viewAllRoles();
+              break;
+  
+            case "View all employees":
+               viewAllEmployees();
+               break;
+  
+            case "Add a department":
+              addDepartment();
+              break;
+  
+            case "Add a role":
+              addRole();
+              break;
+  
+            case "Add an employee":
+                addEmployee();
+                break;
+  
+            case "Update an employee role":
+                updateEmployeeRole();
+                break;
+            
+            case "Exit":
+                console.log("Bye!");
+                connection.end();
+                break
+      }
+    })
+};
+
+ startEmployeeTracker();
